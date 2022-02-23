5b8e56e2c31649b447a79f0fe58ae35df52b2448
Alessio Caruso <caruso.alessio1999@gmail.com>
2022-02-23 17:27:03 +0100

    added part of the ticket in html and added function on js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,44 @@
 document.getElementById("create").addEventListener('click', function(){
-  const fullName= document.getElementById("name");
-  const numberOfKm= document.getElementById("km");
-  const eta = document.getElementById("eta");
-  console.log(fullName.value);
-  console.log(numberOfKm.value);
-  console.log(eta.value);
+  const fullName= document.getElementById("name").value;
+  const numberOfKm= parseInt( document.getElementById("km").value);
+  const eta = document.getElementById("eta").value;
+  console.log(fullName);
+  console.log(numberOfKm);
+  console.log(eta);
 
   let priceTicket = numberOfKm * 0.27;
   let discount;
+  let offering="Biglietto standard";
+  document.getElementById("name-passenger").innerHTML=fullName;
+
+  if ( isNaN(numberOfKm))
+  {
+    console.warn("devi inserire solo i numeri");
+    document.getElementById("price").innerHTML= "Hai inserito dei caratteri non validi, ricarica la pagina";
+  }
+  else
+  {
+    if (eta == "underage")
+    {
+      discount = ((priceTicket/100)*17);
+      priceTicket-= discount ;
+      offering="Biglietto per minorenni"
+    }
+    else if (eta == "elderly")
+    {
+      discount = ((priceTicket/100)*33);
+      priceTicket -= discount;
+      offering="Biglietto per gli over 65"
+    }
+    document.getElementById("cost").innerHTML=priceTicket.toFixed(2) + "€";
+    document.getElementById("offering").innerHTML=offering;
+  
+  } 
+  console.log(priceTicket);
+  fullName.value="";
+  eta.value="";
 
 })
 
 
-// document.getElementById("km").innerHTML= numberOfKm + "km";
-// document.getElementById("eta").innerHTML=eta;
 
-// if ( isNaN(numberOfKm))
-// {
-//   console.warn("devi inserire solo i numeri");
-//   document.getElementById("price").innerHTML= "Hai inserito dei caratteri non validi, ricarica la pagina";
-// }
-// else
-// {
-//   if (eta < 18)
-//   {
-//     discount = ((priceTicket/100)*17);
-//     priceTicket-= discount ;
-//   }
-//   else if (eta >= 65)
-//   {
-//     discount = ((priceTicket/100)*33);
-//     priceTicket -= discount;
-//   }
-//   document.getElementById("price").innerHTML=priceTicket.toFixed(2) + "€";
-// }
